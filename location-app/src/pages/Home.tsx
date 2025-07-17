6a1f03d464f67595bb3697edacb2b0cbf58376b5
heritier <hedemaga@gmail.com>
2025-07-17 23:38:34 +0100

    modification de page Home

--- location-app/src/pages/Home.tsx
+++ location-app/src/pages/Home.tsx
@@ -1,7 +1,16 @@
+import HouseCard from "../components/HouseCard";
+import { houses } from "../data/house";
+
 export default function Home(){
     return(
-        <div className="p-4">
-            <h1 className="text-3xl font-bold">Maison à louer </h1>
+        <div className="space-y-6">
+            <h1 className="text-3xl font-bold text-center text-gray-800">Maison à louer </h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                {houses.map(house => (
+                    <HouseCard key={house.id} house={house} />
+                ))}
+
+            </div>
             </div>
     )
 }

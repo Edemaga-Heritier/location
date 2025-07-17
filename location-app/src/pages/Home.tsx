ae6923583adfbfb149789ac3243a1cfd3820d98a
heritier <hedemaga@gmail.com>
2025-07-17 15:58:50 +0100

    first commit

---
+++ location-app/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+    return(
+        <div className="p-4">
+            <h1 className="text-3xl font-bold">Maison à louer </h1>
+            </div>
+    )
+}
---
+++ location-app/src/pages/HouseDetails.tsx
@@ -0,0 +1,7 @@
+export default function HouseDetails() {
+    return(
+        <div className="p-4">
+            <h1 className="text-2xl font-semibold">Détails de la maison</h1>
+            </div>
+    )
+}

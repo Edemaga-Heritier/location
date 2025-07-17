d3c0faaa62f36479ca053ce78671a8d5cdbaeb43
heritier <hedemaga@gmail.com>
2025-07-17 22:59:40 +0100

    ajouter le data

---
+++ location-app/src/data/house.ts
@@ -0,0 +1,28 @@
+import type { House } from "../types/house";
+
+export const houses:House[] =[
+    {
+        id:1,
+        title:"villa moderne avec piscine",
+        location:"kinshasa",
+        price:1500,
+        image:"https://source.unsplash.com/featured/?house",
+        description:"Une villa moderne avec piscine située à Kinshasa, idéale pour les vacances."
+    },
+    {
+        id:2,
+        title:"appartement cosy en ville",
+        location:"kinshasa",
+        price:800,
+        image:"https://source.unsplash.com/featured/?apartment",
+        description:"Un appartement cosy en plein centre de Kinshasa, parfait pour un séjour urbain."
+    },
+    {
+        id:3,
+        title:"maison familiale spacieuse",
+        location:"kinshasa",
+        price:1200,
+        image:"https://source.unsplash.com/featured/?family-house",
+        description:"Une maison spacieuse pour les familles, avec un grand jardin et proche des écoles."
+    }
+]
---
+++ location-app/src/types/house.ts
@@ -0,0 +1,8 @@
+export interface House{
+    id:number
+    title:string
+    location:string
+    price:number
+    image:string
+    description:string
+}

d28799c0e27280d78f739f3b5e6411524e74ffcb
heritier <hedemaga@gmail.com>
2025-07-28 13:00:57 +0100

    ajoute de composant About et contact

--- location-app/src/App.tsx
+++ location-app/src/App.tsx
@@ -4,6 +4,8 @@ import './App.css'
 import MainLayouts from './layouts/MainLayouts'
 import Home from './pages/Home'
 import HouseDetails from './pages/HouseDetails'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 function App() {
 
@@ -13,6 +15,10 @@ function App() {
       <Route element={<MainLayouts />}>
       <Route path="/" element={<Home />} />
       <Route path="/house/:id" element={<HouseDetails />} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/contact" element={<Contact/>} />
+      <Route path="*" element={<div>Page not found</div>} />
+
       </Route>
     </Routes>
   )
---
+++ location-app/src/pages/About.tsx
@@ -0,0 +1,15 @@
+export default function About (){
+    return(
+        <section className="container mx-auto px-4 py-8">
+            <h1 className="text-3xl font-bold text-bleu-600 mb-6">
+                A propos de Lokaz
+            </h1>
+            <p className="text-gray-700 mb-4 text-lg leading-relaxed">
+                Lokaz est une plateforme de location de maisons et d'appartements qui facilite la recherche de logements pour les voyageurs et les locataires. Notre mission est de rendre la location de biens immobiliers simple, rapide et accessible à tous.
+            </p>
+            <p className="text-gray-700 mb-4 text-lg leading-relaxed">
+            Notre mission est de connecter les propriétaires et les locataires à travers une interface moderne,rapide et sécurisée. Que vous soyez un propriétaire cherchant à louer votre bien ou un locataire à la recherche de votre prochain logement, Lokaz est là pour vous accompagner.
+            </p>
+        </section> 
+    )
+}

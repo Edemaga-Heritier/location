1a1772c9c60faa432a5a0f37bae7ccfba19e250e
heritier <hedemaga@gmail.com>
2025-08-07 20:24:29 +0100

    ajouiter la logique de controlle

--- location-app/backend_location/location_api/app/controllers/houses_controller.ts
+++ location-app/backend_location/location_api/app/controllers/houses_controller.ts
@@ -1,4 +1,61 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import House from '#models/house'
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class HousesController {
+  async index({ response }: HttpContext) {
+    const houses = await House.all()
+    return response.ok(houses)
+  }
+  async show({ params, response }: HttpContext) {
+    const house = await House.find(params.id)
+    if (!house) {
+      return response.notFound({ message: 'House not found' })
+    }
+    return response.ok(house)
+  }
+  async store({ request, response }: HttpContext) {
+    const data = request.only([
+      'title',
+      'description',
+      'price',
+      'location',
+      'surface',
+      'bedrooms',
+      'bathrooms',
+      'image',
+      'has_garden',
+      'has_garage',
+    ])
+    const house = await House.create(data)
+    return response.created(house)
+  }
+  async update({ params, request, response }: HttpContext) {
+    const house = await House.find(params.id)
+    if (!house) {
+      return response.notFound({ message: 'House not found' })
+    }
+    const data = request.only([
+      'title',
+      'description',
+      'price',
+      'location',
+      'surface',
+      'bedrooms',
+      'bathrooms',
+      'image',
+      'has_garden',
+      'has_garage',
+    ])
+    house.merge(data)
+    await house.save()
+    return response.ok(house)
+  }
+  async destroy({ params, response }: HttpContext) {
+    const house = await House.find(params.id)
+    if (!house) {
+      return response.notFound({ message: 'House not found' })
+    }
+    await house.delete()
+    return response.ok({ message: 'Maison supprime avec succes' })
+  }
 }
--- location-app/backend_location/location_api/app/models/house.ts
+++ location-app/backend_location/location_api/app/models/house.ts
@@ -5,6 +5,31 @@ export default class House extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
+  @column()
+  public title!: string
+
+  @column()
+  public description!: string
+
+  @column()
+  public price!: number
+
+  @column()
+  public location!: string
+
+  @column()
+  public surface!: number
+  @column()
+  public bedrooms!: number
+  @column()
+  public bathrooms!: number
+  @column()
+  public image!: string
+  @column()
+  public has_garden!: boolean
+  @column()
+  public has_garage!: boolean
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 

0176dc5b21d60fd043e4d4360628ca93281aee7f
heritier <hedemaga@gmail.com>
2025-08-07 20:38:34 +0100

    ajouter de route sur back-end

--- location-app/backend_location/location_api/start/routes.ts
+++ location-app/backend_location/location_api/start/routes.ts
@@ -9,8 +9,13 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+const HousesController = () => import('#controllers/houses_controller')
+router
+  .group(() => {
+    router.get('/', [HousesController, 'index'])
+    router.get('/:id', [HousesController, 'show'])
+    router.post('/', [HousesController, 'store'])
+    router.put('/:id', [HousesController, 'update'])
+    router.delete('/:id', [HousesController, 'destroy'])
+  })
+  .prefix('/houses')

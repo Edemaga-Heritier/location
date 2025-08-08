edb742d7ca659d4724419c59da49321f43f3419f
heritier <hedemaga@gmail.com>
2025-08-08 20:46:27 +0100

    ajoute la logique dans route

--- location-app/backend_location/location_api/start/routes.ts
+++ location-app/backend_location/location_api/start/routes.ts
@@ -19,3 +19,18 @@ router
     router.delete('/:id', [HousesController, 'destroy'])
   })
   .prefix('/houses')
+//Auth Google
+const AuthController = () => import('#controllers/auth_controller')
+router.get('/auth/google', [AuthController, 'redirectToGoogle'])
+router.get('/auth/google/callback', [AuthController, 'handleGoogleCallback'])
+
+// Create Houses Controller
+const CreateHousesController = () => import('#controllers/create_houses_controller')
+router
+  .group(() => {
+    router.post('/houses', [CreateHousesController, 'store'])
+    router.get('/houses', [CreateHousesController, 'index'])
+    router.put('/houses/:id', [CreateHousesController, 'update'])
+    router.delete('/houses/:id', [CreateHousesController, 'destroy'])
+  })
+  .prefix('/api')
